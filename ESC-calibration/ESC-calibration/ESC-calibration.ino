0ae4180e582146d87bee1bf6fec898ed96ac3aa5
aferrero2707 <aferrero1975@gmail.com>
2021-02-06 23:25:41 +0100

    Updated ESC calibration code

--- ESC-calibration/ESC-calibration/ESC-calibration.ino
+++ ESC-calibration/ESC-calibration/ESC-calibration.ino
@@ -1,13 +1,11 @@
 #define STATUS_LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 #define ERROR_LED_PIN 12 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
-unsigned long loop_timer, loop_timer_stop, loop_count = 0, elapsed, mpuLoopCount = 0;
+unsigned long loop_timer, loop_timer_stop, loop_count = 0;
 
 // ESC variables
 unsigned long esc_loop_timer;
-unsigned long timer_esc_1, timer_esc_2, timer_esc_3, timer_esc_4;
-unsigned long esc_1, esc_2, esc_3, esc_4;
-unsigned long escFL, escFR, escBL, escBR, throttle;
+unsigned long escLength, escTimer;
 
 #define HB_TIMEOUT 60
 void heartBeat()
@@ -30,28 +28,21 @@ void heartBeat()
 void escSignalStart()
 { 
   PORTD |= B11110000;                                                       //Set digital outputs 4,5,6 and 7 high.
-  timer_esc_1 = esc_1 + loop_timer;                                     //Calculate the time of the faling edge of the esc-1 pulse.
-  timer_esc_2 = esc_2 + loop_timer;                                     //Calculate the time of the faling edge of the esc-2 pulse.
-  timer_esc_3 = esc_3 + loop_timer;                                     //Calculate the time of the faling edge of the esc-3 pulse.
-  timer_esc_4 = esc_4 + loop_timer;                                     //Calculate the time of the faling edge of the esc-4 pulse.
+  escTimer = escLength + loop_timer;                                     //Calculate the time of the faling edge of the esc-1 pulse.
 }
 
 void escSignalWait()
 {
 #ifdef BENCHMARK
   esc_loop_timer = micros();                                              //Read the current time.
-  if ((timer_esc_1 <= esc_loop_timer) || (timer_esc_2 <= esc_loop_timer) || 
-      (timer_esc_3 <= esc_loop_timer) || (timer_esc_4 <= esc_loop_timer)) {
+  if (escTimer <= esc_loop_timer) {
         Serial.println("ESC timeout exceeded!!!");
   }
 #endif
 
   while(PORTD >= 16) {                                                       //Stay in this loop until output 4,5,6 and 7 are low.
     esc_loop_timer = micros();                                              //Read the current time.
-    if(timer_esc_1 <= esc_loop_timer) PORTD &= B11101111;                //Set digital output 4 to low if the time is expired.
-    if(timer_esc_2 <= esc_loop_timer) PORTD &= B11011111;                //Set digital output 5 to low if the time is expired.
-    if(timer_esc_3 <= esc_loop_timer) PORTD &= B10111111;                //Set digital output 6 to low if the time is expired.
-    if(timer_esc_4 <= esc_loop_timer) PORTD &= B01111111;                //Set digital output 7 to low if the time is expired.
+    if(escTimer <= esc_loop_timer) PORTD &= B00001111;                //Set digital output 4 to low if the time is expired.
   }
 }
 
@@ -73,10 +64,7 @@ void setup() {
   loop_count = 0;
 
   // initialize ESC signal lengths to minimum
-  esc_1 = 2000;
-  esc_2 = 2000;
-  esc_3 = 2000;
-  esc_4 = 2000;
+  escLength = 2000;
 
   digitalWrite(STATUS_LED_PIN, LOW);
   digitalWrite(ERROR_LED_PIN, LOW);
@@ -89,12 +77,18 @@ void setup() {
 // ================================================================
 
 void loop() {
+  byte data;
+  if(Serial.available() > 0) {
+    data = Serial.read();                                       //Read the incomming byte.
+    delayMicroseconds(1000);                                    //Wait for any other bytes to come in
+    while(Serial.available() > 0) Serial.read(); //Empty the Serial buffer.
 
-  if(Serial.available() > 0){
-    data = Serial.read();                                                               //Read the incomming byte.
-    delay(100);                                                                         //Wait for any other bytes to come in
-    while(Serial.available() > 0)loop_counter = Serial.read();                          //Empty the Serial buffer.
+    if(data == '1') escLength = 2000;
+    if(data == '2') escLength = 1000;
+    if(data == '3') escLength = 1200;
+  }
 
+  heartBeat();
   
   loop_timer_stop = micros();
   if((loop_timer_stop - loop_timer) >= 4000) {
